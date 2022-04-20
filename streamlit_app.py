8961b78752b7992a18ec6f31fdb23f01bd237a1c
Ahmed Ewis <65680238+AhmedEwis@users.noreply.github.com>
2022-04-20 21:56:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import urllib
-!pip install pympler
+#!pip install pympler
 import pympler
 from pympler import asizeof
 from pympler import muppy
